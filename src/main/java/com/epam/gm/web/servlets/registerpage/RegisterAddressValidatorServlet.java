7724a669a0dac8144e6fe9aed473884dc712dc55
olgmaks <drolgmaks@gmail.com>
2015-06-14 17:17:56 +0300

    RegisterAddressValidatorServlet - gryn

---
+++ src/main/java/com/epam/gm/web/servlets/registerpage/RegisterAddressValidatorServlet.java
@@ -0,0 +1,22 @@
+package com.epam.gm.web.servlets.registerpage;
+
+import java.io.IOException;
+import java.sql.SQLException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.epam.gm.web.servlets.frontcontroller.HttpRequestHandler;
+
+public class RegisterAddressValidatorServlet implements HttpRequestHandler {
+
+	@Override
+	public void handle(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException, SQLException {
+
+		
+		
+	}
+
+}
