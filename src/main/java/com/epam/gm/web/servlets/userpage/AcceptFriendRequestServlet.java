a12f7ea093c251c341ab07f167d6436e892dc9f3
olgmaks <drolgmaks@gmail.com>
2015-06-18 15:08:03 +0300

    added frontcontroller.properties a few of servlet for user page. Designed sevlets to remove friend, submit/decline/callback friends requests

---
+++ src/main/java/com/epam/gm/web/servlets/userpage/AcceptFriendRequestServlet.java
@@ -0,0 +1,19 @@
+package com.epam.gm.web.servlets.userpage;
+
+import com.epam.gm.web.servlets.frontcontroller.HttpRequestHandler;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.sql.SQLException;
+
+/**
+ * Created by OLEG on 18.06.2015.
+ */
+public class AcceptFriendRequestServlet implements HttpRequestHandler{
+    @Override
+    public void handle(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException, SQLException {
+
+    }
+}
