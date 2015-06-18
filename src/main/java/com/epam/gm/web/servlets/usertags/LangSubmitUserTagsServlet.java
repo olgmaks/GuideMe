1695c61398c7a9a149db60cf821995d4126c0ce1
olgmaks <drolgmaks@gmail.com>
2015-06-18 19:57:47 +0300

    LangAutocompleteServlet - gryn

---
+++ src/main/java/com/epam/gm/web/servlets/usertags/LangSubmitUserTagsServlet.java
@@ -0,0 +1,21 @@
+package com.epam.gm.web.servlets.usertags;
+
+import java.io.IOException;
+import java.sql.SQLException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.epam.gm.web.servlets.frontcontroller.HttpRequestHandler;
+
+public class LangSubmitUserTagsServlet implements HttpRequestHandler {
+
+	@Override
+	public void handle(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException, SQLException {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
